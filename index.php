75ace16b2eeae685db033781100c435cc085e03a
gc-werlich <31598651+gc-werlich@users.noreply.github.com>
2017-09-18 12:50:38 -0300

    cabecalho

--- index.php
+++ index.php
@@ -105,10 +105,10 @@ if (isset($_POST['submit'])) {
 </head>
 <body>
 
-<!-- Navigation -->
+<!-- Navigation 
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-        <!--<a class="navbar-brand" href="index.html">Start Bootstrap</a>-->
+        <!--<a class="navbar-brand" href="index.html">Start Bootstrap</a> 
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
                 data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -126,7 +126,7 @@ if (isset($_POST['submit'])) {
             </ul>
         </div>
     </div>
-</nav>
+</nav>-->
 
 <!-- Page Header -->
 <header class="masthead" style="background-image: url('img/home2.jpg')">
