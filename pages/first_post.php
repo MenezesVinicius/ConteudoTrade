67de61af30ec271a2c408ed5d864c2835dd118b1
Vinicius Menezes <vicmen33@hotmail.com>
2017-09-19 02:26:55 -0300

    Envio de email com link implementado

--- pages/first_post.php
+++ pages/first_post.php
@@ -187,13 +187,13 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Obrigado!</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Sucesso!</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    Inscrição feita com sucesso! Bem vindo a nossa newsletter!
+                    Seu ebook foi enviado para o seu email! Aproveite a leitura!
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -201,7 +201,7 @@
             </div>
         </div>
     </div>
-
+    
     <div class="modal fade" id="errorModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
          aria-hidden="true">
         <div class="modal-dialog" role="document">
@@ -221,7 +221,7 @@
             </div>
         </div>
     </div>
-    
+
 </div>
 <!-- /.container -->
 
